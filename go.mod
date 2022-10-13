996a4fed09cd739206b733ba5f3aa47648e6303a
viaSSH <hanseung0609@gmail.com>
2022-10-13 13:44:02 +0900

    s3 download code add

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module s3-test
 go 1.18
 
 require (
+	github.com/aws/aws-sdk-go v1.44.114 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/labstack/echo/v4 v4.9.1 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.11 // indirect
@@ -10,7 +12,7 @@ require (
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.1 // indirect
 	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
-	golang.org/x/net v0.0.0-20211015210444-4f30a5c0130f // indirect
-	golang.org/x/sys v0.0.0-20211103235746-7861aae1554b // indirect
+	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
 	golang.org/x/text v0.3.7 // indirect
 )
--- main.go
+++ main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"net/http"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/labstack/echo/v4"
+)
+
+const (
+	AWS_S3_REGION = "ap-northeast-2"
+	AWS_S3_BUCKET = "ssh-dump-files"
 )
 
 func main() {
@@ -20,10 +29,43 @@ func main() {
 	})
 
 	e.GET("/:key", func(c echo.Context) error {
-		return c.JSON(http.StatusOK, map[string]string{
-			"message": "health",
+
+		bucket := "ssh-dump-files"
+		key := "iu/" + c.Param("key")
+
+		session, err := session.NewSession(&aws.Config{Region: aws.String(AWS_S3_REGION)})
+		if err != nil {
+			// log.Fatal(err)
+			fmt.Println(err)
+		}
+
+		svc := s3.New(session)
+
+		result, err := svc.GetObject(&s3.GetObjectInput{
+			Bucket: &bucket,
+			Key:    &key,
 		})
+		if err != nil {
+			fmt.Printf("Failed to get data to %s/%s, %s\n", bucket, key, err)
+			return c.JSON(http.StatusBadRequest, map[string]string{
+				"message": c.Param("key"),
+			})
+		}
+		// datas := []byte(result.Body)
+		// testbytes := []byte
+		// buff := &aws.WriteAtBuffer{}
+
+		// buff.ReadFrom(result.Body)
+		buf := new(bytes.Buffer)
+		_, _ = buf.ReadFrom(result.Body)
+
+		return c.Blob(200, "image/jpg", buf.Bytes())
+
+		// return c.JSON(http.StatusOK, map[string]string{
+
+		// 	"message": c.Param("key"),
+		// })
 	})
 
-	e.Logger.Debug(e.Start(":8090"))
+	e.Logger.Debug(e.Start(":8080"))
 }
